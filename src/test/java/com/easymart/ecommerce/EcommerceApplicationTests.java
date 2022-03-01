663a77407dcebb56b1926d8c3239b7fc5758580c
Lucas Lauro <lucas.vilela@scale.com.br>
2022-03-01 15:47:03 -0300

    first commit

---
+++ src/test/java/com/easymart/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easymart.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
